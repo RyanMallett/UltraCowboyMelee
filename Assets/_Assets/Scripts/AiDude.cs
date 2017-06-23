e87a42fe36bdb6f03c0a7d9e776bc5deea7c6474
Ryan Mallett <ryan.mallett@hotmail.com>
2017-06-23 14:27:23 +1000

    a

---
+++ Assets/_Assets/Scripts/AiDude.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiDude : MonoBehaviour {
+
+    GameObject player;
+    Player playerScript;
+
+    public float aiMoveSpeed;
+
+	// Use this for initialization
+	void Start () {
+        player =  GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, player.gameObject.transform.position, aiMoveSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/_Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    public AIDudeSpawner[] spawners = new AIDudeSpawner[] { };
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Submit")) {
+            spawners[(int)Random.Range(0, (float)spawners.Length)].SpawnDude();
+        }
+	}
+}
