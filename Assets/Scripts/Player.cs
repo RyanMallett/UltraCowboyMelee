266fc57894147b8aaf3f133d2c98ee337dbc1eb5
Ryan Mallett <ryan.mallett@hotmail.com>
2017-06-23 12:47:56 +1000

    Character controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    CharacterController controller;
+    Camera cam;
+
+    public float moveSpeed = 10.0f;
+
+    public float holdSize = 1;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+        cam = GetComponentInChildren<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movementVector = new Vector3(horizontal, 0, vertical);
+        controller.SimpleMove(movementVector * moveSpeed * Time.deltaTime);
+
+        cam.gameObject.transform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Mouse X"));
+        cam.gameObject.transform.Rotate(transform.right * -Input.GetAxis("Mouse Y"));
+	}
+}
